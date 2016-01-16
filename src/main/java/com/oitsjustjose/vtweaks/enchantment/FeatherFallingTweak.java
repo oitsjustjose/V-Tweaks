291b576c3fd6abe9a59ab0a275687ce60952e838
oitsjustjose <stovallj1995@gmail.com>
2016-01-16 11:28:34 -0500

    Adjusted Better Feather Falling for @asiekierka

--- src/main/java/com/oitsjustjose/vtweaks/enchantment/FeatherFallingTweak.java
+++ src/main/java/com/oitsjustjose/vtweaks/enchantment/FeatherFallingTweak.java
@@ -2,20 +2,34 @@ package com.oitsjustjose.vtweaks.enchantment;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class FeatherFallingTweak
 {
 	@SubscribeEvent
-	public void registerTweak(PlayerEvent event)
+	public void registerTweak(LivingHurtEvent event)
 	{
-		if (event.entityPlayer.getCurrentArmor(0) == null)
+		if (!(event.entity instanceof EntityPlayer))
 			return;
-		ItemStack boots = event.entityPlayer.getCurrentArmor(0);
-		int EnchantmentLevelArmor = EnchantmentHelper.getEnchantmentLevel(Enchantment.featherFalling.effectId, boots);
-		if (boots != null && EnchantmentLevelArmor >= 4)
-			event.entityPlayer.fallDistance = 0.0F;
+
+		EntityPlayer player = (EntityPlayer) event.entity;
+
+		if (player.getCurrentArmor(0).getMetadata() > player.getCurrentArmor(0).getMaxDamage() || player.getCurrentArmor(0).stackSize != 1)
+			player.setCurrentItemOrArmor(1, null);
+
+		ItemStack boots = player.getCurrentArmor(0);
+
+		if (boots != null && EnchantmentHelper.getEnchantmentLevel(Enchantment.featherFalling.effectId, boots) >= 4)
+			if (event.source == DamageSource.fall)
+			{
+				boots.damageItem((int) event.ammount, player);
+				event.ammount = 0.0F;
+				if (boots.getMetadata() > boots.getMaxDamage() || boots.stackSize != 1)
+					player.setCurrentItemOrArmor(1, null);
+			}
 	}
 }
