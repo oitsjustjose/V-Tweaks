3eeb32f47f3e231ed4d033cf13133faa20541ddf
oitsjustjose <stovallj1995@gmail.com>
2016-01-15 23:56:25 -0500

    Drop (most) of the skulls back if dim=0

--- src/main/java/com/oitsjustjose/vtweaks/event/mobtweaks/MobKiller.java
+++ src/main/java/com/oitsjustjose/vtweaks/event/mobtweaks/MobKiller.java
@@ -4,9 +4,13 @@ import com.oitsjustjose.vtweaks.util.Config;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.boss.EntityWither;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityGhast;
 import net.minecraft.entity.monster.EntityPigZombie;
 import net.minecraft.entity.passive.EntityBat;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
@@ -38,8 +42,18 @@ public class MobKiller
 		if (toKill != null && toKill instanceof EntityWither && Config.noOverworldWither)
 		{
 			EntityWither wither = (EntityWither) toKill;
-			if(wither.worldObj.provider.getDimensionId() == 0)
+			if (wither.worldObj.provider.getDimensionId() == 0)
+			{
 				wither.setDead();
+				if (!event.entity.worldObj.isRemote)
+				{
+					EntityItem soulSand = new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, new ItemStack(Blocks.soul_sand, 2));
+					EntityItem skulls = new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, new ItemStack(Items.skull, 1, 1).setStackDisplayName("No Withers in the Overworld"));
+					
+					event.entity.getEntityWorld().spawnEntityInWorld(soulSand);
+					event.entity.getEntityWorld().spawnEntityInWorld(skulls);
+				}
+			}
 		}
 	}
 }
