b63bf057eb4d6f811b45628c2ba9c455962fd216
oitsjustjose <stovallj1995@gmail.com>
2016-01-27 16:12:22 -0500

    Refactored Reflections!

--- src/main/java/com/oitsjustjose/vtweaks/event/blocktweaks/SoundTweaks.java
+++ src/main/java/com/oitsjustjose/vtweaks/event/blocktweaks/SoundTweaks.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemDoor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -15,43 +14,33 @@ import net.minecraftforge.fml.relauncher.ReflectionHelper;
 public class SoundTweaks
 {
 	@SubscribeEvent
-	public void registerTweak(PlayerInteractEvent event)
+	public void registerTweak(PlayerInteractEvent event) throws IllegalArgumentException, IllegalAccessException
 	{
 		if (event.action != event.action.RIGHT_CLICK_BLOCK || event.world.getBlockState(event.pos) == null)
 			return;
 
-		// This is a variable created for.. caching? Yeah. Caching
-		ArrayList<ItemStack> cachedItem = new ArrayList<ItemStack>();
+		// Creative Mode Item Cache
+		ArrayList<ItemStack> cache = new ArrayList<ItemStack>();
 		EntityPlayer player = event.entityPlayer;
 		World world = event.world;
 		ItemStack heldItem = player.getHeldItem();
 
 		if (heldItem != null && heldItem.getItem() instanceof ItemDoor)
 		{
-			cachedItem.add(heldItem);
-			Block block = Blocks.planks;
+			cache.add(heldItem);
 			ItemDoor door = (ItemDoor) heldItem.getItem();
-			try
+			Block block = (Block) ReflectionHelper.findField(ItemDoor.class, "block").get(door);
+			
+			if (door.onItemUse(heldItem, player, world, event.pos, event.face, event.pos.getX(), event.pos.getY(), event.pos.getZ()))
 			{
-				block = (Block) ReflectionHelper.findField(ItemDoor.class, "block").get(door);
-				if (door.onItemUse(heldItem, player, world, event.pos, event.face, event.pos.getX(), event.pos.getY(), event.pos.getZ()))
+				if (player.capabilities.isCreativeMode)
 				{
-					if (player.capabilities.isCreativeMode)
-					{
-						player.inventory.addItemStackToInventory(new ItemStack(cachedItem.get(0).getItem(), 1, cachedItem.get(0).getMetadata()));
-						cachedItem.clear();
-					}
-					world.playSoundEffect(event.pos.getX(), event.pos.getY(), event.pos.getZ(), block.stepSound.getPlaceSound(), 1.0F, block.stepSound.getFrequency() - 0.25F);					
-					player.swingItem();
+					player.inventory.addItemStackToInventory(cache.get(0).copy());
+					cache.get(0).stackSize = 1;
+					cache.clear();
 				}
-			}
-			catch (IllegalArgumentException e)
-			{
-				e.printStackTrace();
-			}
-			catch (IllegalAccessException e)
-			{
-				e.printStackTrace();
+				world.playSoundEffect(event.pos.getX(), event.pos.getY(), event.pos.getZ(), block.stepSound.getPlaceSound(), 1.0F, block.stepSound.getFrequency() - 0.25F);
+				player.swingItem();
 			}
 		}
 	}
