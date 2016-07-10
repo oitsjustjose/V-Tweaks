eb31fe634dd6f4c5150bbd3b5e98935ee76d59c6
oitsjustjose <stovallj1995@gmail.com>
2016-07-10 07:53:02 -0500

    Updater support? Maybe?

--- src/main/java/com/oitsjustjose/vtweaks/VTweaks.java
+++ src/main/java/com/oitsjustjose/vtweaks/VTweaks.java
@@ -40,6 +40,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
+@Mod(modid = VTweaks.MODID, name = VTweaks.NAME, version = VTweaks.VERSION, guiFactory = VTweaks.GUIFACTORY, acceptedMinecraftVersions = "1.9.4", updateJSON = "")
 public class VTweaks
 {
 	public static final String MODID = "VTweaks";
