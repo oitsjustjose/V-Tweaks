0c51795e987587df7f6128c81dc213748313c034
oitsjustjose <stovallj1995@gmail.com>
2017-03-12 13:36:46 -0400

    Helper Classes + Bugfix

--- src/main/java/com/oitsjustjose/vtweaks/enchantment/EnchantmentLumbering.java
+++ src/main/java/com/oitsjustjose/vtweaks/enchantment/EnchantmentLumbering.java
@@ -6,19 +6,13 @@ import com.oitsjustjose.vtweaks.VTweaks;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.AnvilUpdateEvent;
-import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EnchantmentLumbering extends Enchantment
@@ -63,7 +57,7 @@ public class EnchantmentLumbering extends Enchantment
 		ItemStack left = event.getLeft();
 		ItemStack right = event.getRight();
 
-		if (!hasLumbering(right) || !(left.getItem() instanceof ItemTool))
+		if (!Enchantments.bookHasEnchantment(right, this) || !(left.getItem() instanceof ItemTool))
 			return;
 
 		Set<String> classes = ((ItemTool) left.getItem()).getToolClasses(left);
@@ -72,26 +66,5 @@ public class EnchantmentLumbering extends Enchantment
 			event.setCost(0);
 			event.setOutput(left.copy());
 		}
-	}
-
-	boolean hasLumbering(ItemStack stack)
-	{
-		if (stack.getItem() == Items.ENCHANTED_BOOK)
-		{
-			NBTTagCompound comp = stack.serializeNBT();
-			if (comp != null)
-			{
-				comp = comp.getCompoundTag("tag");
-				final NBTTagList list = comp.getTagList("StoredEnchantments", 10);
-				for (int i = 0; i < list.tagCount(); i++)
-				{
-					comp = (NBTTagCompound) list.get(i);
-					if (comp.getInteger("id") == 236 && comp.getInteger("lvl") > 0)
-						return true;
-				}
-			}
-
-		}
-		return false;
 	}
 }
--- src/main/java/com/oitsjustjose/vtweaks/enchantment/Enchantments.java
+++ src/main/java/com/oitsjustjose/vtweaks/enchantment/Enchantments.java
@@ -3,6 +3,10 @@ package com.oitsjustjose.vtweaks.enchantment;
 import com.oitsjustjose.vtweaks.VTweaks;
 
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ResourceLocation;
 
 public class Enchantments
@@ -34,14 +38,121 @@ public class Enchantments
 			lumbering = new EnchantmentLumbering().setName(VTweaks.MODID + ":lumbering");
 		}
 	}
-	
+
+	/**
+	 * @param enchName
+	 *            The string version of the Resource Location
+	 * @return The enchantment attached to the resource location
+	 */
 	public static Enchantment getEnchantment(String enchName)
 	{
 		return Enchantment.REGISTRY.getObject(new ResourceLocation(enchName));
 	}
-	
+
+	/**
+	 * @param modid
+	 *            The mod owning the enchantment
+	 * @param enchName
+	 *            The internalized name of the enchantment
+	 * @return The enchantment attached to the resource location
+	 */
 	public static Enchantment getEnchantment(String modid, String enchName)
 	{
 		return Enchantment.REGISTRY.getObject(new ResourceLocation(modid, enchName));
+	}
+
+	/**
+	 * @param bookStack
+	 *            ItemStack containing the enchantetd book
+	 * @param enchantment
+	 *            The enchantment being matched
+	 * @return true if the book has said enchantment, false otherwise
+	 */
+	public static boolean bookHasEnchantment(ItemStack bookStack, Enchantment enchantment)
+	{
+		int id = Enchantment.getEnchantmentID(enchantment);
+
+		if (bookStack.getItem() == Items.ENCHANTED_BOOK)
+		{
+			NBTTagCompound comp = bookStack.serializeNBT();
+			if (comp != null)
+			{
+				comp = comp.getCompoundTag("tag");
+				final NBTTagList list = comp.getTagList("StoredEnchantments", 10);
+				for (int i = 0; i < list.tagCount(); i++)
+				{
+					comp = (NBTTagCompound) list.get(i);
+					if (comp.getInteger("id") == id && comp.getInteger("lvl") > 0)
+					{
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param bookStack
+	 *            ItemStack containing the enchantetd book
+	 * @param enchName
+	 *            The internalized name of the enchantment
+	 * @return true if the book has said enchantment, false otherwise
+	 */
+	public static boolean bookHasEnchantment(ItemStack bookStack, String enchName)
+	{
+		int id = Enchantment.getEnchantmentID(Enchantment.REGISTRY.getObject(new ResourceLocation(enchName)));
+
+		if (bookStack.getItem() == Items.ENCHANTED_BOOK)
+		{
+			NBTTagCompound comp = bookStack.serializeNBT();
+			if (comp != null)
+			{
+				comp = comp.getCompoundTag("tag");
+				final NBTTagList list = comp.getTagList("StoredEnchantments", 10);
+				for (int i = 0; i < list.tagCount(); i++)
+				{
+					comp = (NBTTagCompound) list.get(i);
+					if (comp.getInteger("id") == id && comp.getInteger("lvl") > 0)
+					{
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param bookStack
+	 *            ItemStack containing the enchantetd book
+	 * @param modid
+	 *            The mod owning the enchantment
+	 * @param enchName
+	 *            The internalized name of the enchantment
+	 * @return true if the book has said enchantment, false otherwise
+	 */
+	public static boolean bookHasEnchantment(ItemStack bookStack, String modid, String enchName)
+	{
+		int id = Enchantment.getEnchantmentID(Enchantment.REGISTRY.getObject(new ResourceLocation(modid, enchName)));
+
+		if (bookStack.getItem() == Items.ENCHANTED_BOOK)
+		{
+			NBTTagCompound comp = bookStack.serializeNBT();
+			if (comp != null)
+			{
+				comp = comp.getCompoundTag("tag");
+				final NBTTagList list = comp.getTagList("StoredEnchantments", 10);
+				for (int i = 0; i < list.tagCount(); i++)
+				{
+					comp = (NBTTagCompound) list.get(i);
+					if (comp.getInteger("id") == id && comp.getInteger("lvl") > 0)
+					{
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
 }
