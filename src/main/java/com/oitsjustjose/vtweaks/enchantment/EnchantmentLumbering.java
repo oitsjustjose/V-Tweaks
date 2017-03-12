e9109ef538d0b2c3b6cc841efa9aa11368c3e3ad
oitsjustjose <stovallj1995@gmail.com>
2017-03-12 15:11:06 -0400

    Should fully fix

--- src/main/java/com/oitsjustjose/vtweaks/enchantment/EnchantmentLumbering.java
+++ src/main/java/com/oitsjustjose/vtweaks/enchantment/EnchantmentLumbering.java
@@ -7,7 +7,6 @@ import com.oitsjustjose.vtweaks.VTweaks;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 import net.minecraft.util.ResourceLocation;
@@ -33,19 +32,13 @@ public class EnchantmentLumbering extends Enchantment
 	@Override
 	public boolean canApply(ItemStack stack)
 	{
-		return true;
-	}
+		if (stack.getItem() instanceof ItemTool)
+		{
+			Set<String> classes = ((ItemTool) stack.getItem()).getToolClasses(stack);
+			return classes.contains("axe");
+		}
 
-	@Override
-	public int getMinEnchantability(int par1)
-	{
-		return 0;
-	}
-
-	@Override
-	public int getMaxEnchantability(int par1)
-	{
-		return 0;
+		return false;
 	}
 
 	@SubscribeEvent
