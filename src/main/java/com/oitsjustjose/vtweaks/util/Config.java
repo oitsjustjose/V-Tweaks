e2db3c47e04b93af9004b39e6587f091610b6fb5
oitsjustjose <stovallj1995@gmail.com>
2017-03-16 00:03:58 -0400

    Fun new feature :D

--- src/main/java/com/oitsjustjose/vtweaks/util/Config.java
+++ src/main/java/com/oitsjustjose/vtweaks/util/Config.java
@@ -30,6 +30,7 @@ public class Config
 	public boolean enableMobDropBuffsSkeletons;
 	public boolean enableMobDropBuffsSquids;
 	public boolean enableMobDropBuffsEndermen;
+	public boolean enableHorseGlue;
 	public boolean enableFeatherPlucker;
 	public boolean enableChallengerMobs;
 	public String[] challengerMobDefaultLoot = new String[] { "minecraft:gold_ingot", "minecraft:gold_nugget*15", "minecraft:diamond", "minecraft:emerald", "minecraft:ghast_tear", "minecraft:ender_pearl", "minecraft:emerald", "minecraft:experience_bottle", "minecraft:record_13", "minecraft:record_cat", "minecraft:record_blocks", "minecraft:record_chirp", "minecraft:record_far", "minecraft:record_mall", "minecraft:record_mellohi", "minecraft:record_stal", "minecraft:record_strad", "minecraft:record_ward", "minecraft:record_11", "minecraft:record_wait" };
@@ -148,6 +149,11 @@ public class Config
 		property = config.get(category, "Endermen Drop Extra Ender Pearls", true);
 		property.setComment("If set to false, enderman drops will be unchanged");
 		enableMobDropBuffsEndermen = property.getBoolean();
+		propertyOrder.add(property.getName());
+
+		property = config.get(category, "Horses drop \"glue\" rarely with a Fire Aspect weapon", true);
+		property.setComment("If set to false, horse drops will be unchanged");
+		enableHorseGlue = property.getBoolean();
 		propertyOrder.add(property.getName());
 
 		property = config.get(category, "Pluck Feathers from Chickens", true).setRequiresMcRestart(true);
