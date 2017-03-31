769ce7d7e9e919e370056d4b8556dc95198a07bd
oitsjustjose <jose@Joses-MacBook-Pro.local>
2017-03-30 22:24:37 -0400

    Generalized Config Parsing

--- src/main/java/com/oitsjustjose/vtweaks/util/ConfigParser.java
+++ src/main/java/com/oitsjustjose/vtweaks/util/ConfigParser.java
@@ -7,9 +7,7 @@ import org.apache.logging.log4j.Logger;
 
 import com.oitsjustjose.vtweaks.VTweaks;
 
-import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 
 public class ConfigParser
 {
@@ -17,132 +15,66 @@ public class ConfigParser
 
 	public static void parseItems()
 	{
-		ArrayList<ItemStack> stackList = new ArrayList<ItemStack>();
-
 		LOGGER.info(">> Running Config Item Parser");
-
-		for (String s : VTweaks.config.challengerMobLoot)
-		{
-			// Splits the string apart by uncommon characters
-			// Formatted as <modid>:<item>:<metadata>*<quantity>, <modid>:<item>*quantity, or <modid>:<item>
-
-			String[] parts = s.split("[\\W]");
-
-			if (parts.length == 4)
-			{
-				ItemStack temp = HelperFunctions.findItemStack(parts[0], parts[1]);
-
-				if (temp != null)
-				{
-					try
-					{
-						int meta = Integer.parseInt(parts[2]);
-						int qty = Integer.parseInt(parts[3]);
-						temp.setItemDamage(meta);
-						temp.stackSize = qty;
-						stackList.add(temp.copy());
-					}
-					catch (NumberFormatException e)
-					{
-						LOGGER.info("There was a number formatting issue with entry: " + s + ". It has been skipped.");
-					}
-				}
-			}
-			else if (parts.length == 3)
-			{
-				ItemStack temp = HelperFunctions.findItemStack(parts[0], parts[1]);
-
-				if (temp != null)
-				{
-					try
-					{
-						int qty = Integer.parseInt(parts[2]);
-						temp.stackSize = qty;
-						stackList.add(temp.copy());
-					}
-					catch (NumberFormatException e)
-					{
-						LOGGER.info("There was a number formatting issue with entry: " + s + ". It has been skipped.");
-					}
-
-				}
-			}
-			else if (parts.length == 2)
-			{
-				ItemStack temp = HelperFunctions.findItemStack(parts[0], parts[1]);
-
-				if (temp != null)
-				{
-					stackList.add(temp.copy());
-				}
-			}
-			// The string was not formatted correctly. User is notified.
-			else
-			{
-				LOGGER.info("There was an error parsing item " + s + " as a challenger mob drop. Please confirm that your formatting is correct.");
-			}
-		}
-
+		VTweaks.config.setChallengerLootTable(parse(VTweaks.config.challengerMobLoot, true));
 		LOGGER.info(">> Config Item Parsing complete!");
-		VTweaks.config.setChallengerLootTable(stackList);
 	}
 
 	public static void parseBlocks()
 	{
-		ArrayList<ItemStack> stackList = new ArrayList<ItemStack>();
-
 		LOGGER.info(">> Running Config Block Parser");
+		VTweaks.config.setLavaLossBlockList(parse(VTweaks.config.lavaLossWhitelist, false));
+		LOGGER.info(">> Config Block Parsing complete!");
+	}
 
-		for (String s : VTweaks.config.lavaLossWhitelist)
+	/**
+	 * @param input
+	 *            A string array gathered from a config file
+	 * @param includesQty
+	 *            true if strings in the array include quantities of the itemstack
+	 * @return an ArrayList containing parsed ItemStacks of these
+	 */
+	private static ArrayList<ItemStack> parse(String[] input, boolean includesQty)
+	{
+		ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+		// Goes through every string in the inputs list
+		for (String toSplit : input)
 		{
-			// Splits the string apart by uncommon characters
-			// Formatted as <modid>:<item>:<metadata>*<quantity>, <modid>:<item>*quantity, or <modid>:<item>
-			String[] parts = s.split("[\\W]");
-
-			if (parts.length == 2)
-			{
-				Block b = Block.REGISTRY.getObject(new ResourceLocation(parts[0], parts[1]));
-				if (b != null)
-				{
-					stackList.add(new ItemStack(b));
-					continue;
-				}
-				else
-				{
-					LOGGER.info("Could not add entry " + parts[0] + ":" + parts[1]);
-				}
-			}
-
-			if (parts.length == 3)
+			String[] parts = toSplit.split("[\\W]");
+			ItemStack tempStack = HelperFunctions.findItemStack(parts[0], parts[1]);
+			if (tempStack != null)
 			{
 				try
 				{
-					Block b = Block.REGISTRY.getObject(new ResourceLocation(parts[0], parts[1]));
-					if (b != null)
+					if (parts.length == 2)
 					{
-						stackList.add(new ItemStack(b, 1, Integer.parseInt(parts[2])));
+						ret.add(tempStack.copy());
 						continue;
+					}
+					else if (parts.length == 3)
+					{
+						tempStack = new ItemStack(tempStack.getItem(), 1, Integer.parseInt(parts[2]));
+						ret.add(tempStack.copy());
+					}
+					else if (parts.length == 4 && includesQty)
+					{
+						tempStack = new ItemStack(tempStack.getItem(), Integer.parseInt(parts[3]), Integer.parseInt(parts[2]));
+						ret.add(tempStack.copy());
 					}
 					else
 					{
-						LOGGER.info("Could not add entry " + parts[0] + ":" + parts[1] + ":" + parts[2]);
+						LOGGER.info("Error parsing " + toSplit + ", please ensure proper formatting!");
+						continue;
 					}
 				}
 				catch (NumberFormatException e)
 				{
-					LOGGER.info("There was an error parsing item " + s + ", please ensure this formatting is correct");
+					LOGGER.info("Error parsing " + toSplit + ", please ensure proper formatting!");
+					continue;
 				}
 			}
-
-			// The string was not formatted correctly. User is notified.
-			else
-			{
-				LOGGER.info("There was an error parsing item " + s + " as a challenger mob drop. Please confirm that your formatting is correct.");
-			}
 		}
-
-		LOGGER.info(">> Config Block Parsing complete!");
-		VTweaks.config.setLavaLossBlockList(stackList);
+		return ret;
 	}
 
 }
