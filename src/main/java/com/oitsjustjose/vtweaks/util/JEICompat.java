14b0a83e15c3e007e42a56bc8e9cf57bb04db85b
oitsjustjose <stovallj1995@gmail.com>
2018-08-28 13:17:11 -0500

    JEI Recipe changes to reflect new axe compat

--- src/main/java/com/oitsjustjose/vtweaks/util/JEICompat.java
+++ src/main/java/com/oitsjustjose/vtweaks/util/JEICompat.java
@@ -8,8 +8,13 @@ import mezz.jei.api.recipe.IVanillaRecipeFactory;
 import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 @JEIPlugin
 public class JEICompat implements IModPlugin
@@ -38,7 +43,24 @@ public class JEICompat implements IModPlugin
         }
         if (ModConfig.enchantments.enableLumbering)
         {
-            addAnvilRecipe(factory, registry, new ItemStack(Items.WRITABLE_BOOK), new ItemStack(Items.GOLDEN_AXE), HelperFunctions.getEnchantedBook(Enchantments.getInstance().lumbering));
+            ArrayList<ItemStack> axes = new ArrayList<>();
+            axes.add(new ItemStack(Items.GOLDEN_AXE));
+
+            if (Loader.isModLoaded("toolbox"))
+            {
+                for (String s : new String[]{"null", "diamond", "emerald", "quartz", "prismarine", "ender_pearl", "lapis", "biotite", "amethyst", "ruby", "peridot", "topaz", "tanzanite", "malachite", "sapphire", "amber", "obsidian", "aquamarine"})
+                {
+                    NBTTagCompound comp = new NBTTagCompound();
+                    comp.setString("ADORNMENT", s);
+                    comp.setString("Haft", "wood");
+                    comp.setString("Handle", "wood");
+                    comp.setString("Head", "gold");
+                    ItemStack toolboxAxe = new ItemStack(Objects.requireNonNull(ForgeRegistries.ITEMS.getValue(new ResourceLocation("toolbox", "axe"))));
+                    toolboxAxe.setTagCompound(comp);
+                    axes.add(toolboxAxe);
+                }
+            }
+            addAnvilRecipe(factory, registry, new ItemStack(Items.WRITABLE_BOOK), axes, HelperFunctions.getEnchantedBook(Enchantments.getInstance().lumbering));
         }
     }
 
@@ -50,10 +72,20 @@ public class JEICompat implements IModPlugin
 
     private void addAnvilRecipe(IVanillaRecipeFactory factory, IModRegistry registry, ItemStack inputLeft, ItemStack inputRight, ItemStack output)
     {
-        ArrayList<ItemStack> rightTemp = new ArrayList<ItemStack>();
-        ArrayList<ItemStack> outputTemp = new ArrayList<ItemStack>();
+        ArrayList<ItemStack> rightTemp = new ArrayList<>();
+        ArrayList<ItemStack> outputTemp = new ArrayList<>();
         rightTemp.add(inputRight);
         outputTemp.add(output);
         registry.addRecipes(ImmutableList.of(factory.createAnvilRecipe(inputLeft, rightTemp, outputTemp)), VanillaRecipeCategoryUid.ANVIL);
+    }
+
+    private void addAnvilRecipe(IVanillaRecipeFactory factory, IModRegistry registry, ItemStack inputLeft, ArrayList<ItemStack> inputRight, ItemStack output)
+    {
+        ArrayList<ItemStack> outputTemp = new ArrayList<>();
+        for (int i = 0; i < inputRight.size(); i++)
+        {
+            outputTemp.add(output);
+        }
+        registry.addRecipes(ImmutableList.of(factory.createAnvilRecipe(inputLeft, inputRight, outputTemp)), VanillaRecipeCategoryUid.ANVIL);
     }
 }
