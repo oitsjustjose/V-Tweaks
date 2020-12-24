b947630c757d07abada2fa0df53edc220e20068b
Jose Stovall <stovallj1995@gmail.com>
2020-12-24 14:43:25 -0600

    Possible fix to ghost challenger mob particles.

--- src/main/java/com/oitsjustjose/vtweaks/common/event/mobtweaks/ChallengerParticles.java
+++ src/main/java/com/oitsjustjose/vtweaks/common/event/mobtweaks/ChallengerParticles.java
@@ -6,54 +6,55 @@ import com.oitsjustjose.vtweaks.VTweaks;
 import com.oitsjustjose.vtweaks.common.config.MobTweakConfig;
 
 import net.minecraft.entity.monster.MonsterEntity;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraftforge.event.TickEvent;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ChallengerParticles {
-    private long lastParticle = System.currentTimeMillis();
+    public static final String CHALLENGER_PARTICLE_KEY = "challenger_mobs_last_particle";
 
     @SubscribeEvent
-    public void registerEvent(TickEvent.WorldTickEvent event) {
-        if (!(System.currentTimeMillis() - lastParticle >= 10L)) {
-            return;
-        }
-
+    public void registerEvent(EntityEvent evt) {
         if (!MobTweakConfig.ENABLE_CHALLENGER_MOBS.get() || !MobTweakConfig.ENABLE_CHALLENGER_PARTICLES.get()) {
             return;
         }
 
-        lastParticle = System.currentTimeMillis();
+        if (evt.getEntity() == null || !evt.getEntity().isAlive()) {
+            return;
+        }
 
-        if (event.world instanceof ServerWorld) {
-            ServerWorld serverWorld = ((ServerWorld) event.world);
-            serverWorld.getEntities().forEach((entity) -> {
-                if (entity instanceof MonsterEntity) {
-                    if (ChallengerMobs.isChallengerMob((MonsterEntity) entity)) {
-                        MonsterEntity monster = (MonsterEntity) entity;
-                        ChallengerMobType type = ChallengerMobs.getChallengerMobType(monster);
+        if (evt.getEntity() instanceof MonsterEntity) {
+            if (ChallengerMobs.isChallengerMob((MonsterEntity) evt.getEntity())) {
+                MonsterEntity monster = (MonsterEntity) evt.getEntity();
+                ChallengerMobType type = ChallengerMobs.getChallengerMobType(monster);
 
-                        Random rand = monster.getRNG();
+                Random rand = monster.getRNG();
+                CompoundNBT comp = monster.getPersistentData();
 
-                        float noiseX = ((rand.nextBoolean() ? 1 : -1) * rand.nextFloat()) / 2;
-                        float noiseZ = ((rand.nextBoolean() ? 1 : -1) * rand.nextFloat()) / 2;
-
-                        double x = monster.getPosX() + noiseX;
-                        double y = rand.nextBoolean() ? monster.getPosY() + (monster.getHeight() / 2)
-                                : monster.getPosY();
-                        double z = monster.getPosZ() + noiseZ;
-
-                        y += rand.nextFloat() + rand.nextInt(1);
-
-                        if (type != null) {
-                            float r = type.getRed();
-                            float g = type.getGreen();
-                            float b = type.getBlue();
-                            VTweaks.proxy.showParticle(x, y, z, r, g, b);
-                        }
+                if (comp.contains(CHALLENGER_PARTICLE_KEY)) {
+                    if (System.currentTimeMillis() - comp.getLong(CHALLENGER_PARTICLE_KEY) < 10L) {
+                        return;
                     }
+                } else {
+                    comp.putLong(CHALLENGER_PARTICLE_KEY, System.currentTimeMillis());
                 }
-            });
+
+                float noiseX = ((rand.nextBoolean() ? 1 : -1) * rand.nextFloat()) / 2;
+                float noiseZ = ((rand.nextBoolean() ? 1 : -1) * rand.nextFloat()) / 2;
+
+                double x = monster.getPosX() + noiseX;
+                double y = rand.nextBoolean() ? monster.getPosY() + (monster.getHeight() / 2) : monster.getPosY();
+                double z = monster.getPosZ() + noiseZ;
+
+                y += rand.nextFloat() + rand.nextInt(1);
+
+                if (type != null) {
+                    float r = type.getRed();
+                    float g = type.getGreen();
+                    float b = type.getBlue();
+                    VTweaks.proxy.showParticle(x, y, z, r, g, b);
+                }
+            }
         }
     }
 }
