95162efc0acdcb84fc0c2b91997f41fa31983853
Jose Stovall <stovallj1995@gmail.com>
2021-01-24 15:23:42 -0600

    Clean up import

--- src/main/java/com/oitsjustjose/vtweaks/common/enchantment/handler/EnchantmentLumberingHandler.java
+++ src/main/java/com/oitsjustjose/vtweaks/common/enchantment/handler/EnchantmentLumberingHandler.java
@@ -11,7 +11,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.ToolType;
