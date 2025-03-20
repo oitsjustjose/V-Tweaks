9f2d6eb9a9bca8698d7c315bc2d162138df998cd
oitsjustjose <stovallj1995@gmail.com>
2025-03-20 14:43:59 -0500

    Startup crash hotfix

--- src/main/java/com/oitsjustjose/vtweaks/common/tweaks/block/PowderSnowParticleHandler.java
+++ src/main/java/com/oitsjustjose/vtweaks/common/tweaks/block/PowderSnowParticleHandler.java
@@ -1,33 +1,20 @@
-/**
- * @NOTE This is not a normal tweak!! It is registered via PowderedSnowMixin#PowderSnowBlock
- *
- * Also ensures that this is treated as a singleton -- if multiple (modded) PowderSnowBlock instances are created, we
- * only want to register this event once.
- *
- * This event is not registered if the mixin config is not enabled
- */
-
 package com.oitsjustjose.vtweaks.common.tweaks.block;
 
+import com.oitsjustjose.vtweaks.common.config.CommonConfig;
+import com.oitsjustjose.vtweaks.common.core.Tweak;
+import com.oitsjustjose.vtweaks.common.core.VTweak;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.PowderSnowBlock;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
-public class PowderSnowParticleHandler {
-    private static boolean hasRegistered = false;
-
-    public PowderSnowParticleHandler() {
-        hasRegistered = true;
-    }
-
-    public static boolean hasRegistered() {
-        return hasRegistered;
-    }
-
+@Tweak(category = "" /* No config */)
+public class PowderSnowParticleHandler extends VTweak {
     @SubscribeEvent
     public void registerEvent(PlayerTickEvent.Post evt) {
+        if (!CommonConfig.EnablePowderedSnowMixin.get()) return;
+
         var player = evt.getEntity();
         var level = player.level();
 
--- src/main/java/com/oitsjustjose/vtweaks/common/tweaks/mixin/PowderedSnowMixin.java
+++ src/main/java/com/oitsjustjose/vtweaks/common/tweaks/mixin/PowderedSnowMixin.java
@@ -1,33 +1,20 @@
 package com.oitsjustjose.vtweaks.common.tweaks.mixin;
 
 import com.oitsjustjose.vtweaks.common.config.CommonConfig;
-import com.oitsjustjose.vtweaks.common.tweaks.block.PowderSnowParticleHandler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.PowderSnowBlock;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.neoforged.neoforge.common.NeoForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PowderSnowBlock.class)
 public abstract class PowderedSnowMixin {
-    @Inject(at = @At("TAIL"), method = "<init>")
-    public void PowderSnowBlock(BlockBehaviour.Properties properties, CallbackInfo ci) {
-        if (CommonConfig.EnablePowderedSnowMixin.get()) {
-            if (!PowderSnowParticleHandler.hasRegistered()) {
-                NeoForge.EVENT_BUS.register(new PowderSnowParticleHandler());
-            }
-        }
-    }
-
     @Inject(at = @At("HEAD"), method = "getCollisionShape", cancellable = true)
     protected void getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> cb) {
         if (CommonConfig.EnablePowderedSnowMixin.get()) {
